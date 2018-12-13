f3a5cfc0a88225d03ae1571b447c2efb8908cfbb
rharish101 <harish.rajagopals@gmail.com>
2018-12-13 21:44:46 +0530

    Added instructions in README; cleaned code for inference; updated requirements.txt

--- inference.py
+++ inference.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 # coding: utf-8
-"""HyperOpt Tree of Parzen Estimators method"""
+"""Do inference to get segmentation using existing model."""
 from __future__ import print_function
 from __future__ import division
 import tensorflow as tf
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from utils import get_datasets, EarlyStopper
 from model import Model
-import os
-import subprocess
-
-# Set single GPU
-proc = subprocess.Popen(["../gpu_num_avail.sh"], stdout=subprocess.PIPE)
-out = proc.stdout.read()
-os.environ["CUDA_VISIBLE_DEVICES"] = out.decode("utf-8").strip().split(",")[0]
 
 parser = ArgumentParser(
-    description="Interactive Medical Image Segmentation for Eye-in-the-Sky",
+    description="Inference using Interactive Medical Image Segmentation",
     formatter_class=ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument(
@@ -31,7 +23,7 @@ parser.add_argument(
     "--save-dir",
     type=str,
     default="./model.ckpt",
-    help="where to store Tensorflow model",
+    help="where the Tensorflow model is stored",
 )
 
 args = parser.parse_args()
@@ -39,14 +31,20 @@ args = parser.parse_args()
 tf.set_random_seed(args.random_seed)
 
 print("Loading dataset...")
-data = get_datasets(
-    args.data_path, 0, 0, 32
-)
+data = get_datasets(args.data_path, 0, 0, 32)
 print("Dataset loaded")
 
-model = Model(data, tf.train.AdamOptimizer(learning_rate=0.0008408132388618728),
-              0.003683848079337278, 0.6275728419832726, tf.nn.elu, 56.89889776692406,
-              45.40359136227982, 0.10805612575300722, build=False)
+model = Model(
+    data,
+    tf.train.AdamOptimizer(learning_rate=0.0008408132388618728),
+    0.003683848079337278,
+    0.6275728419832726,
+    tf.nn.elu,
+    56.89889776692406,
+    45.40359136227982,
+    0.10805612575300722,
+    build=False,
+)
 
 # Limit GPU usage
 gpu_options = tf.GPUOptions(allow_growth=True)
